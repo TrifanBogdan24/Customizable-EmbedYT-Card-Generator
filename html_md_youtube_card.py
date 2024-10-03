b9f949d4a71c4577448d75ce1b3698b01d9af34c
TrifanBogdan24 <trifanbogdan24@gmail.com>
2024-10-03 23:28:18 +0300

    Python script: uses 'request' module to check the thumbnail URL on web

--- html_md_youtube_card.py
+++ html_md_youtube_card.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 from typing import List
-import sys
+import requests
 import re
+import sys
 
 
 
@@ -46,8 +47,9 @@ def get_id_of_youtube_url(url: str) -> str:
     """
     The function is given a string, representing an URL.
     
-    The function matches the string against a few REGEX for YouTube links,
-    and if the URL is a valid YouTube link, the VIDEO_ID will be returned.
+    The function matches the string against a few REGEX for YouTube links.
+    
+    If the URL is a valid YouTube link, the VIDEO_ID will be returned.
     Otherwise, it returns an empty string.
     """
     global rgx_01_YT_video
@@ -142,7 +144,8 @@ def get_id_of_youtube_url(url: str) -> str:
         if match:
             return match.group(1)  # Return the video ID
 
-        print(f"ERR: The provided URL, {url} is not a valid YouTube link!", file = sys.stderr)
+        print(f"ERR: The provided URL '{url}' is not a valid YouTube link!", file = sys.stderr)
+        print(f"Please run '{sys.argv[0]} -r' to see the REGEX that validate the URL.", file=sys.stderr)
         print(f"Please run '{sys.argv[0]} -h' to see the available options.", file=sys.stderr)
 
         # Return an empty string if no match found
@@ -152,14 +155,76 @@ def get_id_of_youtube_url(url: str) -> str:
 
 def get_youtube_thumbnail(VIDEO_ID: str) -> str:
     """
+    The function receives an argument, a VIDEO_ID.
+    Return the URL of the thumbnail associated with that ID.
+
+    The functions also validates the existance of the thumbnail URL on the web
+    using 'request' module.
+
+
     For URL 'https://www.youtube.com/shorts/Nl9pcj79byY?feature=share'
     The VIDEO_ID is 'Nl9pcj79byY'
     THUMBNAIL looks like 'https://img.youtube.com/vi/Nl9pcj79byY/hqdefault.jpg'
 
     Alternatives for `hqdefault.jpg` are: `default.jpg`, `mqdefault.jpg` or `sddefault.jpg`
+
+    Using multiple alternative as fallback URLs.
     """
 
-    return f"https://img.youtube.com/vi/{VIDEO_ID}/hqdefault.jpg"
+    thumbnail_1: str = f"https://img.youtube.com/vi/{VIDEO_ID}/hqdefault.jpg"
+    thumbnail_2: str = f"https://img.youtube.com/vi/{VIDEO_ID}/default.jpg"
+    thumbnail_3: str = f"https://img.youtube.com/vi/{VIDEO_ID}/mqdefault.jpg"
+    thumbnail_4: str = f"https://img.youtube.com/vi/{VIDEO_ID}/sddefault.jpg"
+
+
+    internet_connection = False
+
+    try:
+        if requests.get("https://google.com").ok is True and requests.get("https://www.youtube.com/").ok is True:
+            internet_connection = True
+    except:
+        internet_connection = False
+
+    if internet_connection:
+        # Got internet connection
+        try:
+            if requests.get(thumbnail_1).ok is True:
+                return thumbnail_1
+        except:
+            pass
+        
+        try:
+            if requests.get(thumbnail_2).ok is True:
+                return thumbnail_2
+        except:
+            pass
+
+        try:
+            if requests.get(thumbnail_3).ok is True:
+                return thumbnail_3
+        except:
+            pass
+
+        try:
+            if requests.get(thumbnail_4).ok is True:
+                return thumbnail_4
+        except:
+            print("ERR: Could not find thumnbail for YouTube VIDEO ID = {VIDEO_ID}", file=sys.stdout)
+            print("Would you like to continue generating HTML/MD code with default THUMBNAIL URL? Y/n", end = ' ')
+            while True:
+                user_input = input().strip().lower()
+                if user_input in ['y', 'yes']:
+                    return thumbnail_1
+                elif user_input in ['n', 'no']:
+                    sys.exit(1)
+                else:
+                    print("Unrecognize responsed! Please type 'y' for YES and 'n' for NO")
+    else:
+        # No internet, therefore the first thumbnail URL will be returend
+        return thumbnail_1
+
+
+
 
 
 
@@ -382,11 +447,11 @@ def interactive_mode() -> None:
 
     print("Would you like to include the title in the card? Y/n", end = ' ')
     while True:
-        response = input().strip().lower()
-        if response not in ['y', 'yes', 'n', 'no']:
+        user_input = input().strip().lower()
+        if user_input not in ['y', 'yes', 'n', 'no']:
             print("Unrecognize responsed! Please type 'y' for YES and 'n' for NO")
         else:
-            INLCUDE_TITLE = True if response in ['y', 'yes'] else False
+            INLCUDE_TITLE = True if user_input in ['y', 'yes'] else False
             break
 
     if INLCUDE_TITLE is True:
@@ -431,7 +496,6 @@ def interactive_mode() -> None:
 
 
 def display_used_REGEXs():
-    print(f"\t{sys.argv[0]} will match the provided URL against the following REGEX-s:")
     global rgx_01_YT_video
     global rgx_02_YT_video_at_current_time
     global rgx_03_YT_watch_video
@@ -444,6 +508,7 @@ def display_used_REGEXs():
     global rgx_10_YT_short_with_current_time
     global rgx_11_YT_short_with_current_time_and_with_share
     global full_youtube_regex
+    print(f"{sys.argv[0]} will match the provided URL against the following REGEX-s:")
     print(f"\t-> '{rgx_01_YT_video}'")
     print(f"\t-> '{rgx_02_YT_video_at_current_time}'")
     print(f"\t-> '{rgx_03_YT_watch_video}'")
@@ -457,6 +522,10 @@ def display_used_REGEXs():
     print(f"\t-> '{rgx_11_YT_short_with_current_time_and_with_share}'")
     print(f"\t-> '{full_youtube_regex}'")
     print()
+    print(f"\tIf one of them matches the $URL,")
+    print(f"\tthe HTML code for a clickable YouTube card will be generated.")
+    print()
+    print(f"\tIf none of the above REGEX is matched, the program will exit forcefully, with an ERROR message.")
 
 
 def help_option():
@@ -546,3 +615,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
